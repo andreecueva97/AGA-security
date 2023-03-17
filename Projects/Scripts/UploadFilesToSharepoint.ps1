f1a0b31b7f8017cf7574784d65496f8fc7b8894f
andreecueva97 <60671503+andreecueva97@users.noreply.github.com>
2023-03-17 16:10:21 -0300

    Create UploadFilesToSharepoint.ps1

---
+++ Projects/Scripts/UploadFilesToSharepoint.ps1
@@ -0,0 +1,16 @@
+$URL ="https://ucao365.sharepoint.com/sites/DSAFSDAFSAF"
+$ErrorActionPreference = "Stop"
+$PSDefaultParameterValues["*:ErrorAction"]="Stop"
+
+$emailusername = "AFDASFFDS@uFADSFDFSAF"
+$encrypted = Get-Content C:\Users\pcpc\Downloads\scriptsencrypted_password1.txt | ConvertTo-SecureString
+$credential = New-Object System.Management.Automation.PsCredential($emailusername,$encrypted)
+
+#Use line 10, if you use SharePointPnPPowerShellOnline MODULE, but you use PnPPowerShell not need that
+#Import-Module SharePointPnPPowerShellOnline
+Connect-PnPOnline -Url $URL -Credentials $credential
+$Files = Get-ChildItem "C:\Users\pcpc\Desktop\AA"
+foreach($File in $Files){
+    Add-PnPFile -Folder "Documentos Compartidos" -Path $File.FullName
+
+}
